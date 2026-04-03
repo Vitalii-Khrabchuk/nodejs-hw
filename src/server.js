56d3dfaef5eba1a14b29e50dd04c3a8f4e6155a4
Vitalii Khrabchuk <vitalik_av@o2.pl>
2026-04-03 21:55:22 +0200

    fixserver

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
 
 import 'dotenv/config';
 
@@ -9,15 +10,18 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRouter from './routes/notesRoutes.js';
+import authRouter from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use(logger);
 
 app.use(notesRouter);
+app.use(authRouter);
 
 app.use(notFoundHandler);
 app.use(errors());
@@ -25,5 +29,5 @@ app.use(errorHandler);
 
 await connectMongoDB();
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is runing on port ${PORT}`);
 });
